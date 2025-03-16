b382a2fcce462207e96f15c6b204a8cbc9ab317b
Manuel Cobos <flipper7247@gmail.com>
2025-03-16 07:09:23 -0500

    first commit

---
+++ yum-yum-proyect/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Inicio from './pages/Inicio'
+import Navbar from './components/Nav'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Inicio/>
+    </>
+  )
+}
+
+export default App
---
+++ yum-yum-proyect/src/components/Carrousel.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+
+function Carousel({ images }) { 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentIndex(index);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 5000); // Cambia de slide cada 5 segundos
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div id="default-carousel" className="relative w-full">
+      {/* Carousel wrapper */}
+      <div className="relative overflow-hidden rounded-lg md:h-65">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`duration-700 ease-in-out absolute w-full transition-opacity ${index === currentIndex ? "opacity-100" : "opacity-0"}`}
+            data-carousel-item
+          >
+            <img src={image} className="block w-full" alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+      {/* Slider indicators */}
+      <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            type="button"
+            className={`w-3 h-3 rounded-full ${index === currentIndex ? "bg-white" : "bg-gray-300"}`}
+            aria-current={index === currentIndex}
+            aria-label={`Slide ${index + 1}`}
+            onClick={() => goToSlide(index)}
+          ></button>
+        ))}
+      </div>
+      {/* Slider controls */}
+      <button
+        type="button"
+        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        onClick={prevSlide}
+      >
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
+          </svg>
+          <span className="sr-only">Previous</span>
+        </span>
+      </button>
+      <button
+        type="button"
+        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        onClick={nextSlide}
+      >
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg
+            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 6 10"
+          >
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+          </svg>
+          <span className="sr-only">Next</span>
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default Carousel;
