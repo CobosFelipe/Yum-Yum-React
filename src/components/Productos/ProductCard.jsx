4271179d8b9d93bda6cd5840170435665cfb6d8d
CobosFelipe <flipper7247@gmail.com>
2025-09-29 08:33:00 -0500

    Actualizando tarjetas de productos para consumo de API

--- src/components/Productos/ProductCard.jsx
+++ src/components/Productos/ProductCard.jsx
@@ -9,18 +9,18 @@ const ProductCard = ({ item }) => {
     <div className="flex flex-col items-center justify-center max-w-sm">
       {/* Espacio para la imagen del producto*/}
       <div className="w-64 h-64 bg-gray-300 bg-center bg-cover shadow-md rounded-2xl">
-        <img src={item.imagen} className="w-64 h-64 rounded-2xl" />
+        <img src={item.img} className="w-64 h-64 rounded-2xl" />
       </div>
 
       {/* Contenedor de la data del producto */}
       <div className={`flex flex-col w-55 -mt-10 overflow-hidden min-h-[6rem] bg-white rounded-lg shadow-lg border-1 ${styles.card}`}>
         {/* Nombre del producto */}
         <div className="flex flex-grow items-center justify-center">
-          <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase">{item.nombre}</h3>
+          <h3 className="py-2 font-bold tracking-wide text-center text-gray-800 uppercase">{item.product_name}</h3>
         </div>
         {/* Espacio para el precio del producto y agregar al carrito */}
-        <div className="flex items-center justify-between px-3 py-2">
-          <span className="font-bold text-gray-800">$ {item.precio.toLocaleString("es-CO")}</span>
+        <div className="flex items-center justify-around px-3 py-2">
+          <span className="font-bold text-gray-800">$ {item.price.toLocaleString("es-CO")}</span>
           <Toastify
             text={"Producto agregado al carrito"}
             btnText={"Agregar"}
