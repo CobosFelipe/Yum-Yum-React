965bb2c1b38d4c0dd2c12c213e5520ed838de459
CobosFelipe <flipper7247@gmail.com>
2025-10-20 20:14:26 -0500

    Actualizando logica del contexto de los productos

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,67 +1,73 @@
-import { useState } from "react";
+import { useState, useMemo, useCallback } from "react";
 import CartContext from "./CartContext";
 
-export function CartProvider ({children}){
-  const [cart, setCart] = useState([])
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
 
-  const addToCart = producto => {
-    // Revisar si el productoo ya existe en el carrito
-    const productoInCartIndex = cart.findIndex(item => item.id === producto.id)
+  const addToCart = useCallback(
+    (producto) => {
+      // Revisar si el productoo ya existe en el carrito
+      const productoInCartIndex = cart.findIndex((item) => item.product_id === producto.product_id);
 
-    if (productoInCartIndex >= 0) {
-      const newCart = structuredClone(cart)
-      newCart[productoInCartIndex].cantidad += 1
-      return setCart(newCart)
-    }
-
-    setCart(prevState => ([
-      ...prevState,
-      {
-        ...producto,
-        cantidad: 1
+      if (productoInCartIndex >= 0) {
+        const newCart = structuredClone(cart);
+        newCart[productoInCartIndex].cantidad += 1;
+        return setCart(newCart);
       }
-    ]))
-  }
 
-  const removeToCart = producto => {
-    const productoInCartIndex = cart.findIndex(item => item.id === producto.id)
-  
-    if (productoInCartIndex >= 0) {
-      const newCart = structuredClone(cart)
-      const productoEnCarrito = newCart[productoInCartIndex]
-  
-      if (productoEnCarrito.cantidad > 1) {
-        productoEnCarrito.cantidad -= 1
-      } else {
-        newCart.splice(productoInCartIndex, 1)
+      setCart((prevState) => [
+        ...prevState,
+        {
+          ...producto,
+          cantidad: 1,
+        },
+      ]);
+    },
+    [cart]
+  );
+
+  const removeToCart = useCallback(
+    (producto) => {
+      const productoInCartIndex = cart.findIndex((item) => item.product_id === producto.product_id);
+
+      if (productoInCartIndex >= 0) {
+        const newCart = structuredClone(cart);
+        const productoEnCarrito = newCart[productoInCartIndex];
+
+        if (productoEnCarrito.cantidad > 1) {
+          productoEnCarrito.cantidad -= 1;
+        } else {
+          newCart.splice(productoInCartIndex, 1);
+        }
+
+        setCart(newCart);
       }
-  
-      setCart(newCart)
-    }
-  }
-  
+    },
+    [cart]
+  );
 
-  const deleteToCart = producto => {
-    const newCart = cart.filter(item => item.id !== producto.id)
-    return setCart(newCart)
-  }
-  
+  const deleteToCart = useCallback(
+    (producto) => {
+      const newCart = cart.filter((item) => item.product_id !== producto.product_id);
+      return setCart(newCart);
+    },
+    [cart]
+  );
 
-  const clearCart = () => {
-    setCart([])
-  }
+  const clearCart = useCallback(() => {
+    setCart([]);
+  }, []);
 
-  return (
-    <CartContext.Provider value={{
+  const contextValue = useMemo(
+    () => ({
       cart,
       addToCart,
       removeToCart,
       deleteToCart,
-      clearCart
-    }
+      clearCart,
+    }),
+    [cart, addToCart, removeToCart, deleteToCart, clearCart]
+  );
 
-    }>
-      {children}
-    </CartContext.Provider>
-  )
+  return <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>;
 }
